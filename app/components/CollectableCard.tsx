63b96514e607d1d16dbcd303ee616ab9de1c4819
hebot123 <he.coreyy@gmail.com>
2025-09-23 12:43:35 -0700

    fix: correct img tag syntax in CollectableCard

--- app/components/CollectableCard.tsx
+++ app/components/CollectableCard.tsx
@@ -11,7 +11,7 @@ export interface Collectable {
 export default function CollectableCard({ collectable }: { collectable: Collectable }) {
   return (
     <div className="bg-[#121212] border border-[#00FF66] rounded-lg p-4 flex flex-col items-center text-center hover:shadow-lg transition-shadow">
-    < img
+     <imgmg
         src={collectable.image}
         alt={collectable.name}
         width={200}
