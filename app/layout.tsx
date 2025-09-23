91c7d58d9045c2b79bed2df0f37e095cb2e58515
hebot123 <he.coreyy@gmail.com>
2025-09-23 11:29:54 -0700

    feat: update layout to use Navbar and dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,14 @@
 import './globals.css';
 import { ReactNode } from 'react';
-import Header from '@/app/components/Header';
-import Footer from '@/app/components/Footer';
+import Navbar from '@app/components/Navbar';
+import Footer from '@app/components/Footer';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <head />
-      <body className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-1 container mx-auto px-4 sm:px-6 py-6">
+      <body className="flex flex-col min-h-screen bg-[#0A0A0A] text-white">
+        <Navbar />
+        <main className="flex-1 w-full mx-auto px-4 sm:px-6 lg:px-8">
           {children}
         </main>
         <Footer />
