5d7d9c3145df3a34c3c72ec69063424010fba4f4
hebot123 <he.coreyy@gmail.com>
2025-09-23 21:37:56 -0700

    feat: add NFT videos data for FIFA Collect reel

---
+++ lib/nftVideos.ts
@@ -0,0 +1,94 @@
+export interface NftVideo {
+  link: string;
+  country?: string;
+  description: string;
+  edition?: string;
+}
+
+export const nftVideos: NftVideo[] = [
+  {
+    link: "https://storage.googleapis.com/prod-fifa-cms-storage/015b109b-cb86-4a71-9c41-e1e776c21052.mp4#t=0.001",
+    country: "Argentina",
+    description: "2022 FIFA World Cup Qatar\u2122 \u2013 ARG 3 - 0 CRO, GOAL: Julian Alvarez 69'",
+    edition: "#108 of 160",
+  },
+  {
+    link: "https://storage.googleapis.com/prod-fifa-cms-storage/a92e6418-46c5-47d0-8b83-1b2f1bfb3132.mp4#t=0.001",
+    country: "France",
+    description: "2022 FIFA World Cup Qatar\u2122 \u2013 FRA 4 - 1 AUS, CELEBRATION: Kylian Mbappe 68'",
+    edition: "#87 of 222",
+  },
+  {
+    link: "https://storage.googleapis.com/prod-fifa-cms-storage/752ffcfe-7ccf-42de-8471-b555e5a5277e.mp4#t=0.001",
+    country: "Korea Republic",
+    description: "2002 FIFA World Cup Korea/Japan\u2122 \u2013 KOR 2-1 ITA GOLDEN GOAL: Ahn 117'",
+    edition: "#250 of 425",
+  },
+  {
+    link: "https://storage.googleapis.com/prod-fifa-cms-storage/7ebb508d-8983-438c-9598-11b9a9d01b2b.mp4#t=0.001",
+    country: "Belgium",
+    description: "2018 FIFA World Cup Russia\u2122 \u2013 BEL 3-2 JPN GAME WINNING GOAL: Chadli 90+4'",
+    edition: "#214 of 425",
+  },
+  {
+    link: "https://storage.googleapis.com/prod-fifa-cms-storage/b0e20f06-ec16-45e7-86ed-edec1802f91b.mp4#t=0.001",
+    country: "Brazil",
+    description: "2014 FIFA World Cup Brazil\u2122 \u2013 BRA 3-1 CRO LAST MINUTE GOAL: Oscar 90+1'",
+    edition: "#158 of 425",
+  },
+  {
+    link: "https://storage.googleapis.com/prod-fifa-cms-storage/02b234eb-91ff-454d-8f4a-347c9725cfb6.mp4#t=0.001",
+    country: "Korea Republic",
+    description: "2022 FIFA World Cup Qatar\u2122 \u2013 KOR 2 - 1 POR, PASS: Son Heungmin 90+1'",
+    edition: "#349 of 2493",
+  },
+  {
+    link: "https://storage.googleapis.com/prod-fifa-cms-storage/928bf7f4-8866-457a-906c-321e8a001984.mp4#t=0.001",
+    country: "Netherlands",
+    description: "2022 FIFA World Cup Qatar\u2122 \u2013 NED 2 - 2 ARG, GOAL: Wout Weghorst 90+11'",
+    edition: "#262 of 1947",
+  },
+  {
+    link: "https://storage.googleapis.com/prod-fifa-cms-storage/635f6c10-6122-44c7-bf82-1cd821528cdb.mp4#t=0.001",
+    country: "France",
+    description: "2018 FIFA World Cup Russia\u2122 \u2013 FRA 4 - 3 ARG, Skill: Mbappe 12'",
+    edition: "#251 of 913",
+  },
+  {
+    link: "https://storage.googleapis.com/prod-fifa-cms-storage/20e52334-f52b-4b49-a2c1-1a844273f518.mp4#t=0.001",
+    country: "Manchester City FC",
+    description: "FIFA Club World Cup Saudi Arabia 2023\u2122 \u2013 MCI 4-0 FLU, Goal: Alvarez '88'",
+    edition: "#36 of 4550",
+  },
+  {
+    link: "https://storage.googleapis.com/prod-fifa-cms-storage/6bebcbf2-3ee0-412f-adc5-57965c699855.mp4#t=0.001",
+    country: "Argentina",
+    description: "FIFA World Cup Qatar 2022\u2122 \u2013 POL 0 - 2 ARG, Goal: Julian Alvarez 67’",
+    edition: "#183 of 395",
+  },
+  {
+    link: "https://storage.googleapis.com/prod-fifa-cms-storage/e5d611ab-8ed7-486a-b482-47ccabe46610.mp4#t=0.001",
+    country: "Argentina",
+    description: "2022 FIFA World Cup Qatar\u2122 \u2013 ARG 2 - 1 AUS GOAL: Alvarez 57'",
+    edition: "#99 of 1490",
+  },
+  {
+    link: "https://storage.googleapis.com/prod-fifa-cms-storage/18c4ab00-7761-453e-8e2f-22429b46a499.mp4#t=0.001",
+    country: "France",
+    description: "FIFA World Cup Qatar 2022\u2122 \u2013 ARG 3 - 3 FRA, Penalty: Mbappe 118'",
+    edition: "#349 of 390",
+  },
+  {
+    link: "https://storage.googleapis.com/prod-fifa-cms-storage/301695e8-de1a-42d5-b040-c5666b29c187.mp4#t=0.0011",
+    country: "France",
+    description: "2022 FIFA World Cup Qatar\u2122 \u2013 FRA 3-1 POL LAST MINUTE GOAL: Mbappè 90+1'",
+    edition: "#100 of 150",
+  },
+  {
+    link: "https://storage.googleapis.com/prod-fifa-cms-storage/4f8f2007-4f1e-4f32-82a4-75e8fcbafac9.mp4#t=0.001",
+    description: "Football Unites Europe",
+    edition: "#187 of 200",
+  },
+];
+
+export default nftVideos;
